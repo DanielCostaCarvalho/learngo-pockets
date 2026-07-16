6c3e33a1ffc09bbf4181f34fdfb00094519e612a
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2026-07-16 10:13:53 -0300

    feat(logger): adiciona interfaces

--- logger/pocketlog/logger.go
+++ logger/pocketlog/logger.go
@@ -1,4 +1,23 @@
 package pocketlog
 
 type Logger struct {
+	threshold Level
+}
+
+func New(threshold Level) *Logger {
+	return &Logger{
+		threshold: threshold,
+	}
+}
+
+func (logger *Logger) Debugf(format string, args ...any) {
+	//
+}
+
+func (logger *Logger) Infof(format string, args ...any) {
+	//
+}
+
+func (logger *Logger) Errorf(format string, args ...any) {
+	//
 }
