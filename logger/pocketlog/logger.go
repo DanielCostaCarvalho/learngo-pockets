6ca95c5ce4cfdbd3c4141b471fc793ce8c738e5d
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2026-07-17 10:55:22 -0300

    feat(logger): permite selecionar o output

--- logger/pocketlog/logger.go
+++ logger/pocketlog/logger.go
@@ -1,42 +1,51 @@
 package pocketlog
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 // Logger é usado para fazer log de informações
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
 // New retorna um logger que escreve a partir do nível solicitado
-func New(threshold Level) *Logger {
+// O output padrão é Stdout
+func New(threshold Level, output io.Writer) *Logger {
 	return &Logger{
 		threshold: threshold,
+		output:    output,
 	}
 }
 
 // Debugf formata e exibe mensagem no nível debug
 func (logger *Logger) Debugf(format string, args ...any) {
-	if logger.threshold > LevelDebug {
-		return
-	}
-
-	fmt.Printf(format + "\n", args...)
+	logger.logfByLevel(LevelDebug, format, args...)
 }
 
 // Infof formata e exibe mensagem no nível info
 func (logger *Logger) Infof(format string, args ...any) {
-	if logger.threshold > LevelInfo {
-		return
-	}
-
-	fmt.Printf(format + "\n", args...)
+	logger.logfByLevel(LevelInfo, format, args...)
 }
 
 // Errorf formata e exibe mensagem no nível error
 func (logger *Logger) Errorf(format string, args ...any) {
-	if logger.threshold > LevelError {
+	logger.logfByLevel(LevelError, format, args...)
+}
+
+// logfByLevel verifica se o nível é compativel e formata e envia o log
+// para o output
+func (logger *Logger) logfByLevel(level Level, format string, args ...any) {
+	if logger.threshold > level {
 		return
 	}
 
-	fmt.Printf(format + "\n", args...)
+	if logger.output == nil {
+		logger.output = os.Stdout
+	}
+
+	_, _ = fmt.Fprintf(logger.output, format+"\n", args...)
 }
--- logger/pocketlog/logger_test.go
+++ logger/pocketlog/logger_test.go
@@ -1,9 +1,12 @@
 package pocketlog_test
 
-import "go-pocket-sized-projects/logger/pocketlog"
+import (
+	"go-pocket-sized-projects/logger/pocketlog"
+	"os"
+)
 
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
 
 	debugLogger.Debugf("Teste %s", "debug")
 	// Output: Teste debug
