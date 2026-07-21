f33dc2a6cd5a4e3df374139d84db549f60b9d1e1
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2026-07-21 01:39:35 -0300

    test(logger): usa interface para testes

--- logger/pocketlog/logger_test.go
+++ logger/pocketlog/logger_test.go
@@ -3,11 +3,71 @@ package pocketlog_test
 import (
 	"go-pocket-sized-projects/logger/pocketlog"
 	"os"
+	"testing"
 )
 
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
+	debugLogger := pocketlog.New(
+		pocketlog.LevelDebug,
+		pocketlog.WithOutput(os.Stdout),
+	)
 
 	debugLogger.Debugf("Teste %s", "debug")
 	// Output: Teste debug
+}
+
+type testWriter struct {
+	contents string
+}
+
+func (tw *testWriter) Write(p []byte) (n int, err error) {
+	tw.contents = tw.contents + string(p)
+
+	return len(p), nil
+}
+
+func TestLogger(t *testing.T) {
+	type testCase struct {
+		level    pocketlog.Level
+		expected string
+	}
+
+	const (
+		debugMessage = "debug"
+		infoMessage  = "info"
+		errorMessage = "error"
+	)
+
+	testCases := map[string]testCase{
+		"debug": {
+			pocketlog.LevelDebug,
+			debugMessage + "\n" +
+				infoMessage + "\n" +
+				errorMessage + "\n",
+		},
+		"info": {
+			pocketlog.LevelInfo,
+			infoMessage + "\n" +
+				errorMessage + "\n",
+		},
+		"error": {
+			pocketlog.LevelError,
+			errorMessage + "\n",
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			tw := &testWriter{}
+			logger := pocketlog.New(tc.level, pocketlog.WithOutput(tw))
+
+			logger.Debugf(debugMessage)
+			logger.Infof(infoMessage)
+			logger.Errorf(errorMessage)
+
+			if tw.contents != tc.expected {
+				t.Errorf("Invalid contents. Expected %q, got %q", tc.expected, tw.contents)
+			}
+		})
+	}
 }
